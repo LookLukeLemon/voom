1cf9c72dceb8e6e5b27fd904ecdca90767ccde3c
Luke <looklukelemon@gmail.com>
2022-08-02 18:24:23 +0900

    test: socket test

--- apps/realtime/src/events/events.service.spec.ts
+++ apps/realtime/src/events/events.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { EventsService } from './events.gateway';
+import { EventsGateway } from './events.gateway';
 
 describe('EventsService', () => {
-  let service: EventsService;
+  let service: EventsGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EventsService],
+      providers: [EventsGateway],
     }).compile();
 
-    service = module.get<EventsService>(EventsService);
+    service = module.get<EventsGateway>(EventsGateway);
   });
 
   it('should be defined', () => {
