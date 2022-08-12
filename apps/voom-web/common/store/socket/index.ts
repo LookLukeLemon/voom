194f03e1cb93560ca516c8faa3148b57cb985d5e
Luke <looklukelemon@gmail.com>
2022-08-13 00:28:17 +0900

    fix:cert

--- apps/voom-web/common/store/socket/index.ts
+++ apps/voom-web/common/store/socket/index.ts
@@ -3,6 +3,7 @@ import { io, Socket } from 'socket.io-client';
 
 const socket = io(process.env.NEXT_PUBLIC_API_URL || '', {
   transports: ['websocket'],
+  withCredentials: true,
 });
 
 export const socketAtom = atom<Socket>(socket);
