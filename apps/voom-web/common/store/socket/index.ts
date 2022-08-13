483e3e63234b21d279d452d06e075e5ab9b54c5a
Luke <looklukelemon@gmail.com>
2022-08-13 21:58:58 +0900

    test:ssl

--- apps/voom-web/common/store/socket/index.ts
+++ apps/voom-web/common/store/socket/index.ts
@@ -4,10 +4,11 @@ import { io, Socket } from 'socket.io-client';
 const socket = io(process.env.NEXT_PUBLIC_API_URL || '', {
   transports: ['websocket'],
   withCredentials: true,
+  rejectUnauthorized: false,
 });
 
 socket.on('connect_error', (err) => {
-  console.log(`connect_error due to ${err.message}`);
+  console.log(`connect_error due to ${err}`);
 });
 
 export const socketAtom = atom<Socket>(socket);
